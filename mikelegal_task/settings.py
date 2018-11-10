411d10eff6afe0b6452e3b3c0fc5a72ec99a07f1
Raghuvar Prajapati <raghuvarprajapati@gmail.com>
2018-11-10 16:51:59 +0530

    update file

--- mikelegal_task/settings.py
+++ mikelegal_task/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config(SECRET_KEY)
+SECRET_KEY = '52z4+@7qhyj-a(hr$7b7&_-t7wkjjc0q!c*_bd4j!rlrok1#pv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
